2b223bf65c4fa0906af3d791d5b86d82d1c58f97
NIR <MAILMAIL10@GMAIL.COM>
2021-08-22 11:06:10 +0300

    commit

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,9 +2,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://trackbackend.herokuapp.com/'
+    baseURL: 'https://trackbackend.herokuapp.com',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept' : 'application/json',
+    },
 });
 
+// http://2fea15692065.ngrok.io in development
 instance.interceptors.request.use(
     async (config) => {
         const token = await AsyncStorage.getItem('token');
